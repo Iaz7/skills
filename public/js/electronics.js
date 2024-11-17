98e958f7c83ea957ee68f098fa5d33692a0800c0
iaz07 <iazalzabalza@gmail.com>
2024-11-17 20:57:19 +0100

    Ejercicios 5 y 6

--- public/js/electronics.js
+++ public/js/electronics.js
@@ -1,8 +1,17 @@
+let submissions;
+let submissionsTable;
+let skillId;
+
 window.onload = () => {
     const skillHTML = localStorage.getItem("skill-html");
     const skillName = localStorage.getItem("skill-name");
+    skillId = parseInt(localStorage.getItem("skill-id"));
+
     document.getElementById("container").innerHTML = skillHTML;
     document.querySelector("h1").innerHTML = "Skill: " + skillName;
+
+    submissions = JSON.parse(localStorage.getItem("evidenceSubmissions"));
+    submissionsTable = document.getElementById("submissions-table");
 
     const checkboxes = document.querySelectorAll('.checkbox');
     checkboxes.forEach(checkbox => {
@@ -30,7 +39,7 @@ window.onload = () => {
         }
     };
 
-    const submitButton = document.querySelector('button');
+    const submitButton = document.getElementById("submit")
     submitButton.addEventListener('click', () => {
         submitButton.classList.add('clicked');
         setTimeout(() => {
@@ -39,7 +48,81 @@ window.onload = () => {
 
         const textbox = document.getElementById('textbox');
         if (textbox.value.trim() !== "") {
+            let sumbission = {
+                user: "user",
+                evidence: textbox.value.trim(),
+                status: "pending"
+            }
             textbox.value = "";
+            submissions[skillId].push(sumbission);
+            localStorage.setItem("evidenceSubmissions", JSON.stringify(submissions));
+            loadSubmissions(submissionsTable);
         }
+    });
+
+    loadSubmissions(submissionsTable);
+}
+
+function loadSubmissions() {
+
+    submissionsTable.innerHTML = "<tr>\n" +
+                                 "<th>User</th>\n" +
+                                 "<th>Evidence</th>\n" +
+                                 "<th>Actions</th>\n" +
+                                 "</tr>";
+
+    JSON.parse(localStorage.getItem("evidenceSubmissions"))[skillId].forEach((s, i, arr) => {
+        if (s.status !== "pending") return;
+
+        const row = document.createElement("tr");
+
+        const userCol = document.createElement("td");
+        userCol.innerText = s.user;
+
+        const evidenceCol = document.createElement("td");
+        evidenceCol.innerText = s.evidence;
+
+        const actionsCol = document.createElement("td");
+        const approveButton = document.createElement("button");
+        approveButton.innerText = "Approve";
+        approveButton.classList.add('button-green');
+        approveButton.addEventListener("click", e => {
+            approveButton.classList.add('clicked');
+            setTimeout(() => {
+                approveButton.classList.remove('clicked');
+            }, 200);
+            submissions[skillId][i] = {
+                user: s.user,
+                evidence: s.evidence,
+                status: "approved"
+            };
+            localStorage.setItem("evidenceSubmissions", JSON.stringify(submissions));
+            loadSubmissions();
+        });
+        actionsCol.appendChild(approveButton);
+
+        const rejectButton = document.createElement("button");
+        rejectButton.innerText = "Reject";
+        rejectButton.classList.add('button-red');
+        rejectButton.addEventListener("click", e => {
+            rejectButton.classList.add('clicked');
+            setTimeout(() => {
+                rejectButton.classList.remove('clicked');
+            }, 200);
+            submissions[skillId][i] = {
+                user: s.user,
+                evidence: s.evidence,
+                status: "rejected"
+            };
+            localStorage.setItem("evidenceSubmissions", JSON.stringify(submissions));
+            loadSubmissions();
+        });
+        actionsCol.appendChild(rejectButton);
+
+        row.appendChild(userCol);
+        row.appendChild(evidenceCol);
+        row.appendChild(actionsCol);
+
+        submissionsTable.appendChild(row);
     });
 }
--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,33 @@ window.onload = async () => {
     const competencias = await (await fetch("data/competencias.json")).json();
     const contenedorCompetencias = document.getElementsByClassName("svg-container")[0];
 
+    if (localStorage.getItem("evidenceSubmissions") == null) {
+        localStorage.setItem("evidenceSubmissions", JSON.stringify(Array.from({ length: competencias.length }, () => [])));
+    }
+
     for (let i = 0; i < competencias.length; i++) {
         let wrapperHTML =
-            `<div class="svg-wrapper" data-id="${i+1}" data-custom="false">\n` +
+            `<div class="svg-wrapper" data-id="${i}" data-custom="false">\n` +
             "  <svg width=\"100\" height=\"100\" viewBox=\"0 0 100 100\">\n" +
-            "    <polygon points=\"50,5 95,27.5 95,72.5 50,95 5,72.5 5,27.5\"\n" +
-            "             class=\"hexagon\"/>\n" +
+            "    <defs>\n" +
+            "      <linearGradient id=\"gradient-red\" x1=\"0%\" y1=\"0%\" x2=\"100%\" y2=\"100%\">\n" +
+            "        <stop offset=\"0%\" style=\"stop-color: #ff8787; stop-opacity: 1\" />\n" +
+            "        <stop offset=\"100%\" style=\"stop-color: #d32323; stop-opacity: 1\" />\n" +
+            "      </linearGradient>\n" +
+            "      <linearGradient id=\"gradient-green\" x1=\"0%\" y1=\"0%\" x2=\"100%\" y2=\"100%\">\n" +
+            "        <stop offset=\"0%\" style=\"stop-color: #7efb7e; stop-opacity: 1\" />\n" +
+            "        <stop offset=\"100%\" style=\"stop-color: #397f39; stop-opacity: 1\" />\n" +
+            "      </linearGradient>\n" +
+            "    </defs>\n" +
+
+            (JSON.parse(localStorage.getItem("evidenceSubmissions"))[i].some(submission => submission.user == "user" && submission.status == "approved") ?
+        "    <polygon points=\"50,5 95,27.5 95,72.5 50,95 5,72.5 5,27.5\" class=\"hexagon\" style=\"fill: #5ac35a\" />\n" :
+        "    <polygon points=\"50,5 95,27.5 95,72.5 50,95 5,72.5 5,27.5\" class=\"hexagon\"/>\n") +
+
+            "    <circle cx=\"15\" cy=\"15\" r=\"10\" class=\"hex-circle hex-circle-red\" />\n" +
+            "    <text x=\"15\" y=\"20\" text-anchor=\"middle\" fill=\"white\" font-size=\"10\" font-weight=\"bold\">" + JSON.parse(localStorage.getItem("evidenceSubmissions"))[i].reduce((count, item) => item.status === "pending" ? count + 1 : count, 0) + "</text>\n" +
+            "    <circle cx=\"85\" cy=\"15\" r=\"10\" class=\"hex-circle hex-circle-green\" />\n" +
+            "    <text x=\"85\" y=\"20\" text-anchor=\"middle\" fill=\"white\" font-size=\"10\" font-weight=\"bold\">" + JSON.parse(localStorage.getItem("evidenceSubmissions"))[i].reduce((count, item) => item.status === "approved" ? count + 1 : count, 0) + "</text>\n" +
             "    <text x=\"50%\" y=\"20%\" text-anchor=\"middle\" fill=\"black\" font-size=\"9\">\n";
 
         competencias[i].texto.trim().split("\n\n\n").forEach(texto => {
@@ -23,7 +44,6 @@ window.onload = async () => {
             "</div>";
 
         contenedorCompetencias.innerHTML += wrapperHTML;
-
     }
 
     const svgWrappers = document.getElementsByClassName('svg-wrapper');
@@ -42,11 +62,13 @@ window.onload = async () => {
         const botonCuaderno = wrapper.querySelector(".right-button");
         botonCuaderno.addEventListener("click", e => {
             const skillHTML = e.target.parentElement.getElementsByTagName("svg")[0].outerHTML;
-            const skillName = competencias[parseInt(e.target.parentElement.getAttribute("data-id"))-1].texto;
+            const skillId= e.target.parentElement.getAttribute("data-id");
+            const skillName = competencias[parseInt(skillId)].texto;
 
             localStorage.setItem("skill-html", skillHTML);
             localStorage.setItem("skill-name", skillName);
-            window.open('../users/electronics', '_blank');
+            localStorage.setItem("skill-id", skillId);
+            window.location.href = '../users/electronics';
         });
     }
 }
