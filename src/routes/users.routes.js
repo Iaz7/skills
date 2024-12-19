8454c00d9a0791c94af6ca976ae7799b98ea6a3c
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-12-19 20:27:20 +0100

    Add skill management routes and authentication endpoints

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,19 @@
 const { Router } = require('express');
 const router = Router();
 const ranks = require('../public/data/ranks.json');
+const authController = require('../controllers/auth.controller');
 
-router.get('/leaderboard', (req, res) => {
-    console.log('Acessing /users/leaderboard');
-    res.render('leaderboard', { ranks });
-});
+router.get('/leaderboard', (req, res) => res.render('leaderboard', { ranks }));
+
+// Frontend login & register routes
+router.get('/login', (req, res) => res.render('auth'));
+router.get('/register', (req, res) => res.render('auth'));
+router.get('/logout', (req, res) => authController.logout(req, res));
+
+
+// API login & register routes
+router.post('/login', authController.login);
+router.post('/register', authController.register);
+router.post('/logout', authController.logout);
 
 module.exports = router;
