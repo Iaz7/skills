e35c2c5aa67fd9f6cffb91800ffd2151e90dbf77
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-12-20 20:23:21 +0100

    Rename auth.controller.js to user.controller.js and add user-related functions for registration, login, logout, and leaderboard view

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,28 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
 
+
+/**
+ * Renders the registration form.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
+const showRegisterForm = (req, res) => {
+    res.render('auth/register');
+}
+
+/**
+ * Registers a new user.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.body - The body of the request.
+ * @param {string} req.body.username - The username of the new user.
+ * @param {string} req.body.password - The password of the new user.
+ * @param {string} req.body.pass_confirmation - The password confirmation.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves to void.
+ */
 const register = async (req, res) => {
     const { username, password, pass_confirmation } = req.body;
 
@@ -32,12 +54,33 @@ const register = async (req, res) => {
             case 14031: // OutOfDiskSpace (https://www.mongodb.com/docs/manual/reference/error-codes/#mongodb-error-14031)
                 return res.status(500).json({ error: 'DB_DISK_SPACE', message: 'Out of disk space' });
             default: // Default error
-                return res.status(400).json({ error: err });
+                return res.status(400).json({ error: 'ERR_UNKNOWN', message: `Unknown error: ${err}` });
         };
     };
 };
 
+/**
+ * Renders the login form.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
+const showLoginForm = (req, res) => {
+    res.render('auth/login');
+}
+
+/**
+ * Handles user login.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} req.body - The body of the request.
+ * @param {string} req.body.username - The username of the user.
+ * @param {string} req.body.password - The password of the user.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves to void.
+ */
 const login = async (req, res) => {
+    console.log(req.body);
     const { username, password } = req.body;
 
     if (!username || !password) return res.status(400).json({ erro: 'ERR_EMPTY', message: 'All fields are required' });
@@ -48,15 +91,42 @@ const login = async (req, res) => {
 
     req.session.user = { id: user._id, username: user.username };
 
-    res.status(200).json({ message: 'User logged in successfully' });
-}
-
-const logout = async (req, res) => {
-    req.session.destroy((err) => {
-        if (err) return res.status(500).json({ error: 'ERR_SESSION', message: 'Failed to destroy login session' });
-        res.status(200).json({ message: 'User logged out successfully' });
-    });
     res.redirect('/');
 }
 
-module.exports = { register, login, logout };
+/**
+ * Logs out the user by destroying the session.
+ *
+ * @async
+ * @function logout
+ * @param {Object} req - The request object.
+ * @param {Object} req.session - The session object.
+ * @param {Function} req.session.destroy - The function to destroy the session.
+ * @param {Object} res - The response object.
+ * @param {Function} res.status - The function to set the response status.
+ * @param {Function} res.json - The function to send a JSON response.
+ * @param {Function} res.redirect - The function to redirect the response.
+ * @returns {void}
+ */
+const logout = async (req, res) => {
+    req.session.destroy((err) => {
+        if (err) return res.status(500).json({ error: 'ERR_SESSION', message: 'Failed to destroy login session' });
+        res.redirect('/');
+    });
+}
+
+/**
+ * Controller function to view the leaderboard.
+ * Fetches all users from the database, sorts them by points in descending order,
+ * and renders the 'leaderboard' view with the sorted users.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Promise<void>} - A promise that resolves when the leaderboard is rendered.
+ */
+const viewLeaderboard = async (req, res) => {
+    const users = await User.find().sort({ points: -1 });
+    res.render('leaderboard', { users });
+};
+
+module.exports = { showRegisterForm, register, showLoginForm, login, logout, viewLeaderboard };
