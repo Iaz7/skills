e88e317f477f70907d89062410a4c3dccca8c790
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-12-21 01:33:34 +0100

    Refactor authentication middleware to streamline isAuthenticated and isAdmin functions; improve error handling for unauthorized access

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,17 +1,18 @@
-const isAdmin = (req, res, next) => {
-    if (isAuthenticated(req, res, next) && req.session.user.admin) {
-        return next();
-    } else {
-        res.status(403).render('403', { title: 'Forbidden', message: 'You do not have permission to access this page' });
-    }
-};
-
-const isAuthenticated = (req, res, next) => {
+const isAuthenticated = (req, res, next, fromIsAdmin=false) => {
     if (req.session.user) {
+        if (fromIsAdmin) return true;
         return next();
     } else {
         if (req.originalUrl === '/users/logout') return res.redirect('/users/login'); // Redirect to login page if user is logging out
-        res.status(401).render('401', { title: 'Unauthorized', message: 'You must be logged in to access this page', route: req.originalUrl });
+        res.status(401).render('errors/401', { title: 'Unauthorized', message: 'You must be logged in to access this page', route: req.originalUrl });
+    }
+};
+
+const isAdmin = (req, res, next) => {
+    if (isAuthenticated(req, res, next, true) && req.session.user.admin) {
+        return next();
+    } else {
+        res.status(403).render('errors/403', { title: 'Forbidden', message: 'You do not have permission to access this page', route: req.originalUrl });
     }
 };
 
