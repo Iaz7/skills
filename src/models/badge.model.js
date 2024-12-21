87aaf0b449a05527c2a2eb87c52ee3f41c391502
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-12-21 03:37:14 +0100

    Remove redundant 'id' field from badge schema in Mongoose model

--- src/models/badge.model.js
+++ src/models/badge.model.js
@@ -2,7 +2,6 @@ const { model, Schema } = require('mongoose');
 
 const badgeSchema = new Schema({
     _id: { type: Schema.Types.ObjectId, auto: true },
-    id: { type: Schema.Types.ObjectId, required: true },
     name: { type: String, required: true, unique: true },
     bitpoints_min: { type: Number, required: true },
     bitpoints_max: { type: Number, required: true },
