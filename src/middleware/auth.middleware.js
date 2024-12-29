443fc858579f98b5a87f43b48dd9034ad55ba83e
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-12-29 19:09:15 +0100

    Refactor authentication middleware to simplify redirect logic and improve readability

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,19 +1,14 @@
-const isAuthenticated = (req, res, next, fromIsAdmin=false) => {
-    if (req.session.user) {
-        if (fromIsAdmin) return true;
-        return next();
-    } else {
-        if (req.originalUrl === '/users/logout') return res.redirect('/users/login'); // Redirect to login page if user is logging out
-        return res.redirect(`/users/login?redirect=${encodeURIComponent(req.originalUrl)}`);
-    }
+const isAuthenticated = (req, res, next) => {
+    if (req.session.user) return next();
+
+    const redirectUrl = req.originalUrl !== '/users/logout' ? `/users/login?redirect=${encodeURIComponent(req.originalUrl)}` : '/users/login';
+    return res.redirect(redirectUrl);
 };
 
 const isAdmin = (req, res, next) => {
-    if (isAuthenticated(req, res, next, true) && req.session.user.admin) {
-        return next();
-    } else {
-        res.status(403).render('errors/403', { title: 'Forbidden', message: 'You do not have permission to access this page', route: req.originalUrl });
-    }
+    if (req.session.user?.admin) return next();
+
+    return res.status(403).render('errors/403', { title: 'Forbidden', message: 'You do not have permission to access this page', route: req.originalUrl });
 };
 
 module.exports = { isAdmin, isAuthenticated };
