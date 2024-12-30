05e6ed480efb3aadbe481dba38b4af811db97f98
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-12-30 23:26:39 +0100

    Enhance authentication middleware to support user sessions from both session and request objects

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,12 +1,12 @@
 const isAuthenticated = (req, res, next) => {
-    if (req.session.user) return next();
+    if (req.session.user || req.user) return next();
 
     const redirectUrl = req.originalUrl !== '/users/logout' ? `/users/login?redirect=${encodeURIComponent(req.originalUrl)}` : '/users/login';
     return res.redirect(redirectUrl);
 };
 
 const isAdmin = (req, res, next) => {
-    if (req.session.user?.admin) return next();
+    if (req.session.user?.admin || req.user?.admin) return next();
 
     return res.status(403).render('errors/403', { title: 'Forbidden', message: 'You do not have permission to access this page', route: req.originalUrl });
 };
